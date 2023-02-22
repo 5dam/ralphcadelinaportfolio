784218b1c74a9424bbc50f572308696e572110e5
5dam <ralphcadelina5@gmail.com>
2023-02-22 14:07:22 +0800

    works and about layout

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,17 @@
-import { Container } from "@chakra-ui/react";
+import { Box, Container, Heading } from '@chakra-ui/react'
+
 
 
 export default function About() {
   return (
-    <Container>
-        <h1>About</h1>
-    </Container>
+    <>
+      <Container maxW="container.xl">
+        <Heading as="h1" size="4xl" mt="20" fontWeight="bold">About</Heading>
+      </Container>
+      
+      <Box bg="gray.200" h="100vh" mt={-2}>
+
+      </Box>
+  </>
   )
 }
--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,10 +1,17 @@
-import { Container } from "@chakra-ui/react";
+import { Box, Container, Heading } from '@chakra-ui/react'
+
 
 
 export default function Works() {
   return (
-    <Container>
-        <h1>Works</h1>
-    </Container>
+    <>
+      <Container maxW="container.xl">
+        <Heading as="h1" size="3xl" mt="20" fontWeight="bold">Work Experiences</Heading>
+      </Container>
+    
+      <Box bg="gray.200" h="100vh" mt={-2}>
+
+      </Box>
+    </>
   )
 }
