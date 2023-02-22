d5daadb1bf21b61a4ff2a9b4373b53bce312a484
5dam <ralphcadelina5@gmail.com>
2023-02-22 16:47:41 +0800

    project page layout almost done

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,23 @@
-import { Button, ButtonGroup, Box, Container, Heading, SimpleGrid, Card, CardBody, Image, Stack, Text, Divider, CardFooter  } from '@chakra-ui/react'
+import { 
+    Button, 
+    ButtonGroup, 
+    Box, 
+    Container, 
+    Heading, 
+    SimpleGrid, 
+    Card, 
+    CardBody, 
+    Image, 
+    Stack, 
+    Text, 
+    Divider, 
+    CardFooter, 
+    ListItem,
+    List,  
+    IconButton
+  } 
+  from '@chakra-ui/react'
+import { FaBootstrap, FaCss3,  FaHtml5, FaLaravel, FaPhp, FaReact } from 'react-icons/fa'
 
 
 export default function Projects() {
@@ -9,72 +28,111 @@ export default function Projects() {
       </Container>
       
       <Box bg="gray.200" h="100%" mt={-3}>
-        <SimpleGrid columns={{ base:"1", md:"1", lg:"2"}} p="10" spacing="20">
-          
+        <SimpleGrid columns={{ base:"1", md:"1", lg:"2"}} p="10" spacing="10">
           <Box>
               <Card maxW='container.xl'>
                 <CardBody>
-                  <Image
-                    src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-                    alt='Green double couch with wooden legs'
-                    borderRadius='lg'
-                  />
+                  <a href="#">
+                    <Image
+                      src='/static/images/mkdph-front.png'
+                      alt='MKDPH image'
+                      borderRadius='lg'
+                    />
+                  </a>
                   <Stack mt='6' spacing='3'>
-                    <Heading size='md'>Living room Sofa</Heading>
-                    <Text>
-                      This sofa is perfect for modern tropical spaces, baroque inspired
-                      spaces, earthy toned spaces and for people who love a chic design with a
-                      sprinkle of vintage design.
-                    </Text>
-                    <Text color='blue.600' fontSize='2xl'>
-                      $450
-                    </Text>
-                  </Stack>
-                </CardBody>
-                <Divider />
-                <CardFooter>
-                  <ButtonGroup spacing='2'>
-                    <Button variant='solid' colorScheme='blue'>
-                      Buy now
-                    </Button>
-                    <Button variant='ghost' colorScheme='blue'>
-                      Add to cart
-                    </Button>
-                  </ButtonGroup>
-                </CardFooter>
+                  <Heading size='md'>Mindanao Kokusai Daigaku</Heading>
+                  <Text>
+                   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos temporibus vero ratione vitae maxime officiis repellendus natus vel ducimus eveniet.
+                  </Text>
+                  <Text fontSize='2xl'>
+                    <Text color="blue.400">Technology used : </Text>
+                    <List>
+                      <ListItem fontSize="md">
+                        <IconButton bg="none" mr={3} icon={<FaHtml5 />}/>
+                        HTML
+                      </ListItem>
+                      <ListItem fontSize="md">
+                        <IconButton bg="none" mr={3} icon={<FaCss3 />}/>
+                        CSS
+                      </ListItem>
+                      <ListItem fontSize="md">
+                        <IconButton bg="none" mr={3} icon={<FaBootstrap />}/>
+                        Bootstrap
+                      </ListItem>
+                      <ListItem fontSize="md">
+                        <IconButton bg="none" mr={3} icon={<FaPhp />}/>
+                        PHP
+                      </ListItem>
+                      <ListItem fontSize="md">
+                        <IconButton bg="none" mr={3} icon={<FaLaravel />} />
+                        Laravel
+                      </ListItem>
+                    </List>
+                  </Text>
+                </Stack>
+              </CardBody>
+              <Divider />
+              <CardFooter>
+                <Text>Github repo :</Text>
+                <Text ml={5} as="u"><a href="https://github.com/tsujintsujin/KokusaiDaigaku" target="_blank">https://github.com/tsujintsujin/KokusaiDaigaku</a></Text>                
+              </CardFooter>
+              <CardFooter mt={-7}>
+                <Text>Live link :</Text>
+                <Text ml={12} as="u"><a href="#" target="_blank">Currently Working...</a></Text>
+              </CardFooter>
               </Card>
           </Box>
 
           <Box>
             <Card maxW='container.xl'>
               <CardBody>
-                <Image
-                  src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-                  alt='Green double couch with wooden legs'
-                  borderRadius='lg'
-                />
+                <a href="https://kodego-attendance.vercel.app/" target="_blank">
+                  <Image
+                    src='/static/images/kodegomonitoringsystem-mp2.png'
+                    alt='KodeGo Monitoring System image'
+                    borderRadius='lg'
+                  />
+                </a>
                 <Stack mt='6' spacing='3'>
-                  <Heading size='md'>Living room Sofa</Heading>
+                <Heading size='md'>KodeGo Monitoring System</Heading>
                   <Text>
-                    This sofa is perfect for modern tropical spaces, baroque inspired
-                    spaces, earthy toned spaces and for people who love a chic design with a
-                    sprinkle of vintage design.
+                   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos temporibus vero ratione vitae maxime officiis repellendus natus vel ducimus eveniet.
                   </Text>
-                  <Text color='blue.600' fontSize='2xl'>
-                    $450
+                  <Text fontSize='2xl'>
+                    <Text color="blue.400">Technology used : </Text>
+                    <List>
+                      <ListItem fontSize="md">
+                        <IconButton bg="none" mr={3} icon={<FaHtml5 />}/>
+                        HTML
+                      </ListItem>
+                      <ListItem fontSize="md">
+                        <IconButton bg="none" mr={3} icon={<FaCss3 />}/>
+                        CSS
+                      </ListItem>
+                      <ListItem fontSize="md">
+                        <IconButton bg="none" mr={3} icon={<FaReact />}/>
+                        ReactJS
+                      </ListItem>
+                      <ListItem fontSize="md">
+                        <IconButton bg="none" mr={3}/>
+                        Material UI
+                      </ListItem>
+                      <ListItem fontSize="md">
+                        <IconButton bg="none" mr={3} />
+                        Firebase
+                      </ListItem>
+                    </List>
                   </Text>
                 </Stack>
               </CardBody>
               <Divider />
               <CardFooter>
-                <ButtonGroup spacing='2'>
-                  <Button variant='solid' colorScheme='blue'>
-                    Buy now
-                  </Button>
-                  <Button variant='ghost' colorScheme='blue'>
-                    Add to cart
-                  </Button>
-                </ButtonGroup>
+                <Text>Github repo :</Text>
+                <Text ml={5} as="u"><a href="https://github.com/5dam/kodego-attendance" target="_blank">https://github.com/5dam/kodego-attendance</a></Text>                
+              </CardFooter>
+              <CardFooter mt={-7}>
+                <Text>Live link :</Text>
+                <Text ml={12} as="u"><a href="https://kodego-attendance.vercel.app/" target="_blank">https://kodego-attendance.vercel.app/</a></Text>
               </CardFooter>
             </Card>
           </Box>
@@ -82,33 +140,45 @@ export default function Projects() {
           <Box>
             <Card maxW='container.xl'>
               <CardBody>
-                <Image
-                  src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-                  alt='Green double couch with wooden legs'
-                  borderRadius='lg'
-                />
+                <a href="https://majorproject1-moveme.vercel.app/">
+                  <Image
+                    src='/static/images/moveme-mp1.png'
+                    alt='Green double couch with wooden legs'
+                    borderRadius='lg'
+                  />
+                </a>
                 <Stack mt='6' spacing='3'>
-                  <Heading size='md'>Living room Sofa</Heading>
+                  <Heading size='md'>Move Me</Heading>
                   <Text>
-                    This sofa is perfect for modern tropical spaces, baroque inspired
-                    spaces, earthy toned spaces and for people who love a chic design with a
-                    sprinkle of vintage design.
+                   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos temporibus vero ratione vitae maxime officiis repellendus natus vel ducimus eveniet.
                   </Text>
-                  <Text color='blue.600' fontSize='2xl'>
-                    $450
+                  <Text fontSize='2xl'>
+                    <Text color="blue.400">Technology used : </Text>
+                    <List>
+                      <ListItem fontSize="md">
+                        <IconButton bg="none" mr={3} icon={<FaHtml5 />}/>
+                        HTML
+                      </ListItem>
+                      <ListItem fontSize="md">
+                        <IconButton bg="none" mr={3} icon={<FaCss3 />}/>
+                        CSS
+                      </ListItem>
+                      <ListItem fontSize="md">
+                        <IconButton bg="none" mr={3} icon={<FaBootstrap />}/>
+                        Bootstrap
+                      </ListItem>
+                    </List>
                   </Text>
                 </Stack>
               </CardBody>
               <Divider />
               <CardFooter>
-                <ButtonGroup spacing='2'>
-                  <Button variant='solid' colorScheme='blue'>
-                    Buy now
-                  </Button>
-                  <Button variant='ghost' colorScheme='blue'>
-                    Add to cart
-                  </Button>
-                </ButtonGroup>
+                <Text>Github repo :</Text>
+                <Text ml={5} as="u"><a href="https://github.com/5dam/majorproject1-moveme" target="_blank">https://github.com/5dam/majorproject1-moveme</a></Text>                
+              </CardFooter>
+              <CardFooter mt={-7}>
+                <Text>Live link :</Text>
+                <Text ml={12} as="u"><a href="https://majorproject1-moveme.vercel.app/" target="_blank">https://majorproject1-moveme.vercel.app/</a></Text>
               </CardFooter>
             </Card>
           </Box>
