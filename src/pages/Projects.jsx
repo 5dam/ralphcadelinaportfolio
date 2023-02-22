27cb8f587e3c38b763d2247d23468f75d041c763
5dam <ralphcadelina5@gmail.com>
2023-02-22 14:02:45 +0800

    added project layout

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,16 @@
-import { Container } from '@chakra-ui/react'
+import { Box, Container, Heading } from '@chakra-ui/react'
 
 
 export default function Projects() {
   return (
-    <Container>
-        <h1>Projects</h1>
-    </Container>
+    <>
+      <Container maxW="container.xl">
+        <Heading as="h1" size="4xl" mt="20" fontWeight="bold">Projects</Heading>
+      </Container>
+      
+      <Box bg="gray.200" h="100vh" mt={-2}>
+
+      </Box>
+    </>
   )
 }
